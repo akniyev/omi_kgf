90a317e05fc803799a3414340ea290e331e24041
Hasan A <Hasan A>
2017-03-13 00:53:24 +0300

    I have added some libs and projects from dsp

--- omi_kgf/MainWindow.xaml.cs
+++ omi_kgf/MainWindow.xaml.cs
@@ -12,17 +12,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GraphBuilders;
+using DiscreteFunctionsPlots;
+using static System.Math;
+using DiscreteFunctions;
 
 namespace omi_kgf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>    
+
     public partial class MainWindow : Window
     {
+        GraphBuilder2DForm graphBuilder;
+        Plot2D functionPlot;
+
         public MainWindow()
         {
             InitializeComponent();
+            graphBuilder = new GraphBuilder2DForm();
+            functionPlot = new Plot2D();
+            graphBuilder.GraphBuilder.DrawPlot(functionPlot);
+            graphBuilder.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double[] xs = new double[100];
+            double[] ys = new double[100];
+
+            for (var i = 0; i < 100; i++)
+            {
+                xs[i] = 2 * PI / 100.0 * i;
+                ys[i] = Sin(xs[i]);
+            }
+
+            var discreteFunction = new DiscreteFunction2D(xs, ys);
+
+            functionPlot.DiscreteFunction = discreteFunction;
+
+            functionPlot.Refresh();
         }
     }
 }
